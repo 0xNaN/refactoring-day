4d314b061391e6e3326ff8fc3a57ac8e59290260
0xNaN <enzodicicco@gmail.com>
2015-10-22 16:07:23 +0200

    moving Childern Movie charge strategy to NewChildernMovie and turning Price.getCharge to abstract

--- movie-rental/java/src/main/java/movierental/ChildernPrice.java
+++ movie-rental/java/src/main/java/movierental/ChildernPrice.java
@@ -6,5 +6,13 @@ public class ChildernPrice extends Price {
 	int getPriceCode() {
 		return Movie.CHILDRENS;
 	}
+	
+	@Override
+	public double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+        return result;
+	}
 
 }
--- movie-rental/java/src/main/java/movierental/Price.java
+++ movie-rental/java/src/main/java/movierental/Price.java
@@ -2,16 +2,5 @@ package movierental;
 
 public abstract class Price {
 	abstract int getPriceCode();
-	
-	public double getCharge(int daysRented) {
-    	double result = 0;
-	    switch (getPriceCode()) {
-	        case Movie.CHILDRENS:
-	            result += 1.5;
-	            if (daysRented > 3)
-	                result += (daysRented - 3) * 1.5;
-	            break;
-	    }
-	    return result;
-    }
+	abstract double getCharge(int daysRented);
 }
